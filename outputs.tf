780a737aa64e1467bf341e28bd99671721ffb9f0
Máté Lang <langmatelaszlo@gmail.com>
2023-09-28 11:22:37 +0200

    exposed private,public subnet ids and vpc name

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+
+output "vpc_name" {
+  value = module.vpc.name
+}
